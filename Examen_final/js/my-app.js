4a06f5da796ae162132ba903a7ae6f337d41c1c7
rubenros15 <42951176+rubenros15@users.noreply.github.com>
2018-12-12 20:08:10 -0600

    Add files via upload

---
+++ Examen_final/js/my-app.js
@@ -0,0 +1,40 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/info-cake/',
+    	url: 'info-cake.html',
+    	name: 'info-cake',
+  		},
+		{
+		path: '/cookies/',
+    	url: 'cookies.html',
+    	name: 'cookies',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
